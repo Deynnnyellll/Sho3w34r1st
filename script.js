d67fbc09d39a4e20d3bf44feb2fa3da9d33bcc5b
Deynnnyellll <138265285+Deynnnyellll@users.noreply.github.com>
2023-07-21 00:12:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,19 @@ window.onscroll = function() {
   prevScrollPos = currentScrollPos;
 };
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwoPICHwyR8CmgvEWp6UQF7tNOBetCCITNx3sfShoYYGt4CCdGHnK5WZkIBScP3_6-0/exec'
+const form = document.forms['submit-to-google-sheet']
 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        alert("Thank you for your message!")
+
+        form.reset()
+    } )
+    .catch(error => console.error('Error!', error.message))
+})
 
 
 
